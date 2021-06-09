f5ba99f19fb2c043f233bc23df862479b262b1f1
Prachi2617 <36615941+Prachi2617@users.noreply.github.com>
2021-06-09 20:41:30 +0530

    Update Binary_Tree _Level_Order_Traversal.cpp

--- Binary_Tree _Level_Order_Traversal.cpp
+++ Binary_Tree _Level_Order_Traversal.cpp
@@ -20,10 +20,41 @@ void levelOrder(TreeNode* root)
         }
         return ans;
     }
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+// without using two queue just in single queue we can do this we just have to take the size of queue and then in for loop put child of all those nodes 
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        vector<vector<int>>ans;
+        if(root==NULL)
+            return ans;
+        queue<TreeNode *>q;
+        q.push(root);
+        vector<int>v;
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int i=1;i<=n;i++)
+            {
+                TreeNode *curr=q.front();
+                q.pop();
+                v.push_back(curr->val);
+                if(curr->left)
+                {
+                    q.push(curr->left);
+                }
+                if(curr->right)
+                    q.push(curr->right);
+            }
+            ans.push_back(v);
+            v.clear();
+        }
+        return ans;
+    }
+}
 
-
-
-
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 // This is according to leetcode as we have to return vector of vector so we are using two queues otherwise we can do this with just one queue
 //Here two queue's are used one is to store the current node while other is to store the child of current node
 //vector is used to store all the nodes->vel from that level and when the 1st queue is empty then copy all the nodes to the answer
