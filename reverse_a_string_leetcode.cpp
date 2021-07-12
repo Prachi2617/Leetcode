b22210fbe3d9a9479094d355c92376f0182e763c
Prachi2617 <36615941+Prachi2617@users.noreply.github.com>
2021-07-12 18:22:38 +0530

    Add files via upload

---
+++ reverse_a_string_leetcode.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s)
+    {
+        int i,n;
+        n=s.size();
+        for(i=0;i<n/2;i++)
+        {
+            swap(s[i],s[n-1-i]);
+        }
+    }
+};
