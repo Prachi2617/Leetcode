89c4f6632feff8e11b1e8752260fd2c55cba2aa5
Prachi2617 <36615941+Prachi2617@users.noreply.github.com>
2021-08-19 22:11:14 +0530

    Update min_stack.cpp

--- min_stack.cpp
+++ min_stack.cpp
@@ -1,3 +1,42 @@
+//approach 1: using two vector
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    vector<int> st;
+    vector<int> it;
+   
+    MinStack() {
+       
+    }
+   
+    void push(int val) {
+        st.push_back(val);
+        if(it.empty() || val<=it.back())
+            it.push_back(val);
+    }
+   
+    void pop()
+    {
+        if(it.back()==st.back())
+            it.pop_back();
+        st.pop_back();
+    }
+   
+    int top() {
+        //it=st.begin()+st.size()-1;
+        //return *it;
+        return st.back();
+    }
+   
+    int getMin()
+    {
+        int id=it.back();
+        return id;
+    }
+};
+
+//approach 2: using two stack
+
 class MinStack {
 public:
     /** initialize your data structure here. */
