7348418690747da62d5f5c407e3044a1b5671572
Prachi2617 <36615941+Prachi2617@users.noreply.github.com>
2022-01-20 18:43:50 +0530

    Update foursum.cpp

--- foursum.cpp
+++ foursum.cpp
@@ -1,4 +1,6 @@
- vector<vector<int>> fourSum(vector<int>& nums, int target)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) 
     {
         vector<vector<int>>ans;
         vector<int>dummy;
@@ -9,19 +11,19 @@
         sort(nums.begin(),nums.end());
         for(i=0;i<n-3;i++)
         {
-            if (i > 0 && nums[i] == nums[i-1])
+            if (i > 0 && nums[i] == nums[i-1]) 
                 continue;
-            if(nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target)
+            if(target-nums[i]-nums[i+1]-nums[i+2]-nums[i+3]<0) 
                 break;
-            if(nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<target)
+            if(target -nums[i]-nums[n-3]-nums[n-2]-nums[n-1]>0) 
                 continue;
             for(j=i+1;j<n-2;j++)
             {
-                if (j > (i+1) && nums[j] == nums[j-1])
+                if (j > (i+1) && nums[j] == nums[j-1]) 
                     continue;
-                if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target)
+                if(target - nums[i]- nums[j]- nums[j+1]- nums[j+2]<0) 
                     break;
-                if(nums[i]+nums[j]+nums[n-2]+nums[n-1]<target)
+                if(target-nums[i]-nums[j]-nums[n-2]-nums[n-1]>0) 
                     continue;
                 l=j+1;
                 r=n-1;
@@ -32,15 +34,15 @@
                         l++;
                     else if(s>target)
                         r--;
-                    else
+                    else 
                     {
-                        dummy.push_back(nums[i]);
-                        dummy.push_back(nums[j]);
-                        dummy.push_back(nums[l]);
-                        dummy.push_back(nums[r]);
+                        dummy.push_back(nums[i]); 
+                        dummy.push_back(nums[j]); 
+                        dummy.push_back(nums[l]); 
+                        dummy.push_back(nums[r]); 
                         ans.push_back(dummy);
                         dummy.clear();
-
+                       
                        do{
                            l++;
                        }while(nums[l]==nums[l-1]&&l<r);
@@ -48,9 +50,10 @@
                            r--;
                        }while(nums[r]==nums[r+1]&&l<r);
                     }
-
+                   
                 }
             }
         }
         return ans;
     }
+};
